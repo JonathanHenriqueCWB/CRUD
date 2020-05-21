e18e590db61b37e511741fc1ed0d5067056a5164
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-05-20 21:45:53 -0300

    Criando a classe de contexto

---
+++ Crud.Repository/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Crud.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Repository.Data
+{
+    public class Context : DbContext
+    {        
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> MyProperty { get; set; }
+
+    }
+}
