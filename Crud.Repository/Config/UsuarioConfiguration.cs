4f4757c61ac6cc35ff52b0a06d1de5fdbdec8341
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-05-21 16:11:07 -0300

    Criando as classes de configuração

---
+++ Crud.Repository/Config/UsuarioConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Crud.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Crud.Repository.Config
+{
+    class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(u => u.UsuarioId);
+
+            builder.Property(u => u.Nome).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.Email).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.Senha).IsRequired().HasMaxLength(50);
+        }
+    }
+}
